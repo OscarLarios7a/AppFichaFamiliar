5f202712b3cf59db2506ca0c37be331c53099765
Oscar Larios <javier.atoaxaca@gmail.com>
2022-08-27 18:08:19 -0500

    Agregando los nombres a los Elementos para la Maninupulacion de los datos en el ceDatosTerritorial.cs y cambios en la BD

---
+++ capaEntidad/capaEntidadDatosTerritoriales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidad
+{
+    class capaEntidadDatosTerritoriales
+    {
+        public int idEstructuraTerritorial { get; set; }
+        public int idRegion { get; set; }
+        public string region { get; set; }
+        public string cveLocOfi { get; set; }
+        public string cveMunicipio { get; set; }
+        public string municipio { get; set; }
+        public string cveLocalidad { get; set; }
+        public string localidad { get; set; }
+        public string microZona { get; set; }
+        public int idFamilia { get; set; }
+    }
+}
