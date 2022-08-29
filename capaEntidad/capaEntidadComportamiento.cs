c3832da716b396d7ca04104883b842406c03e376
Oscar Larios <javier.atoaxaca@gmail.com>
2022-08-29 18:12:35 -0500

    Creando mas clases para la capaEntidad

---
+++ capaEntidad/capaEntidadComportamiento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidad
+{
+    public class capaEntidadComportamiento
+    {
+        public int idComportamiento { get; set; }
+        public int idFamilia { get; set; }
+        public string liqSem1_2022 { get; set; }
+        public string liqSem1_2021 { get; set; }
+        public string liqSem2_2021 { get; set; }
+    }
+}
--- capaEntidad/capaEntidadTutora.cs
+++ capaEntidad/capaEntidadTutora.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace capaEntidad
 {
-    class capaEntidadTutora
+    public class capaEntidadTutora
     {
+        public int idTutora { get; set; }
+        public int idFamilia { get; set; }
+        public string idIntegrante { get; set; }
+        public string curpTutora { get; set; }
+        public string  nombreTutora { get; set; }
+        public string apTutora { get; set; }
+        public string amTutora { get; set; }
+        public string situacionTutora { get; set; }
     }
 }
