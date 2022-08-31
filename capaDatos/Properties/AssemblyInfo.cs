7f9ec3e80927007eea55bd5673d540f85c747ee9
Oscar Larios <javier.atoaxaca@gmail.com>
2022-08-31 18:39:26 -0500

    Cambios de Actualizacion en la CapaDatos

--- capaDatos/Properties/AssemblyInfo.cs
+++ capaDatos/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("capaDatos")]
+[assembly: AssemblyTitle("capaData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("capaDatos")]
+[assembly: AssemblyProduct("capaData")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("55ad20c3-5b86-457c-a3fe-53d9b4573c9a")]
+[assembly: Guid("70b559e2-4c6e-4d49-be53-b94b13a393c4")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
---
+++ capaDatos/capaDataConexionBD.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using capaEntidad;
+using System.Data;
+
+namespace capaData
+{
+    public class capaDataConexionBD
+    {
+        string cad_cnx;
+        MySqlConnection mysqlcnn;
+
+        public capaDataConexionBD()
+        {
+            cad_cnx = "Server=localhost;User=root;Password=oportunidades;Port=3306;database=educacionbasica";
+        }
+        //Metodo para la conexion a la BD
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+
+
+                mysqlcnn = new MySqlConnection(cad_cnx);
+                mysqlcnn.Open();
+
+                // mysqlcnn.Open();
+
+            }
+            catch (MySqlException ex)
+            {
+                 MessageBox.Show("Error: Al conectarse a la Base de datos." + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Al realizar la conexion la Base de datos." + ex.Message);
+            }
+           
+            return mysqlcnn;
+        }
+
+        //Metodo para Ejecucion de Consultas
+
+        public int ExecuteQuery(string SQL)
+        {
+            int result = 0;
+            try
+            {
+
+                MySqlCommand MySqlCommand = new MySqlCommand(SQL, GetConnection());
+
+                result = MySqlCommand.ExecuteNonQuery();
+                mysqlcnn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Error: Al ejecutar las sentencias SQL." + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return result;
+        }
+
+
+        public MySqlDataReader GetDataReader(string SQL)
+        {
+            MySqlDataReader MySqlDr = null;
+            try
+            {
+                MySqlCommand MySqlCommand = new MySqlCommand(SQL, GetConnection());
+                MySqlDataAdapter MySqlDap = new MySqlDataAdapter(MySqlCommand);
+                MySqlDr = MySqlCommand.ExecuteReader();
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Error: Al ejecutar las sentencias SQL." + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return MySqlDr;
+        }
+
+
+        public DataTable GetDataTabla(string SQL)
+        {
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            MySqlCommand MySqlCommand = new MySqlCommand(SQL, GetConnection());
+            MySqlDataAdapter MySqlDap = new MySqlDataAdapter(MySqlCommand);
+            MySqlDap.SelectCommand = MySqlCommand;
+            MySqlDap.Fill(ds);
+            dt = ds.Tables[0];
+            return dt;
+
+
+        }
+    }
+}
